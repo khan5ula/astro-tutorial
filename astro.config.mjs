6bbeab01ee8860eea7ef0c33be5269b4d8cb8d07
Kristian <kristian.hannula@proton.me>
2023-11-06 21:59:42 +0200

    Reached part 6

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://stellar-platypus-a47c95.netlify.app/"
+});

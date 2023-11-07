b46599d3984cce872f2e8da4b5e49c8f89c7dffc
Kristian <kristian.hannula@proton.me>
2023-11-07 15:03:20 +0200

    Comleted the blog tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://stellar-platypus-a47c95.netlify.app/"
+  site: "https://stellar-platypus-a47c95.netlify.app/",
+  integrations: [preact()]
 });
